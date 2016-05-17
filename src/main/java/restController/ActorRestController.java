6755fcd2cc1578a25a7fe932f682e28b9cd5be9c
cathlene <cathlene.van.moorleghem@student.khleuven.be>
2016-05-17 11:53:16 +0200

    add crossOrigin

--- src/main/java/restController/ActorRestController.java
+++ src/main/java/restController/ActorRestController.java
@@ -15,11 +15,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 /**
  *
  * @author cathlene
  */
+
+@CrossOrigin
 @RestController
 @RequestMapping("/actor")
 public class ActorRestController {
--- src/main/java/restController/MovieRestController.java
+++ src/main/java/restController/MovieRestController.java
@@ -10,6 +10,7 @@ import domain.Movie;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author cathlene
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/movie")
 public class MovieRestController {
